a06be6ba988cd4cb4a7cd935fc4d0914d27c4ae0
bhanu pratap singh <bhanupratapsingh@bhanus-MacBook-Air.local>
2024-01-26 11:17:03 +0530

    Day 1

---
+++ Day1.py
@@ -0,0 +1,73 @@
+# Day 1 - 100 Days of Code : Print and was mostly about Replit Workspace
+
+
+print('Hello Replit') 
+# Everything in the quotes is text, also called a String
+
+print("and how are you?")
+print("🤗")
+
+
+print("""
+     Here's
+     an 
+     exact
+     way 
+     i 
+     wrote
+     the
+     code
+      """)
+
+# Thats what those three Quotes do
+
+
+# Common Coding Errors
+
+1.  # Print("What could go wrong?")
+    # NameError: name 'Print' is not defined. Did you mean: 'print'?
+
+
+2.  # print "Hello World"
+    # SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+    
+    
+# Fixed my Code
+
+print("What could go wrong?")
+print("Hello World")
+
+
+# print(Please Work)
+
+# Note :- The Solution to the this problem is 
+#         about the syntax as the strings or 
+#         something you wanna print as it is should be under the quotes
+#         doesnt matter whether it is single or double quotes
+
+# Solution
+print("Please Work")
+
+
+########################################################################################
+
+
+# Day 1 - Challenge
+
+# Problem : Print some stuff related to my name and today's date and a piece of text
+
+# Solution
+
+print("""
+Bhanu
+January 26th 2024
+I am signing up for Replit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do 
+this from my phone wherever I happen to be. No excuses 
+for not coding from the middle of a field!
+I am feeling 🥳
+#replit100DaysOfCode
+      """)
+
