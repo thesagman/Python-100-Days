096dac800e3040c0bfa991a626986270b69aa694
bhanu pratap singh <bhanupratapsingh@bhanus-MacBook-Air.local>
2024-01-28 20:13:17 +0530

    Day3 is Done

---
+++ Day3.py
@@ -0,0 +1,33 @@
+# Day 3 - 100 Days of Code : Concatenation
+
+# Concatenation can be referred as simply gluing the text with
+# the variables
+
+myName = input("What is your Name?: ")
+myLunch = input("What are you having for Lunch?: ")
+print(myName,"is going to be chowing down on",myLunch,"very soon")
+
+
+
+# Common Coding Errors
+
+# yourName = input("Name: ")
+# WhatYear = input("What year is it?: ")
+# print(yourName "thinks it is " WhatYear)
+# SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+# Do remember the comma or simply the syntax of concatenation
+
+
+# Day 3 Challenge 
+
+# Solution
+
+name = input("Name a food > ")
+plant = input("Name a type of plant > ")
+method = input("Name a method of cooking > ")
+describe = input("What word describes burned food? > ")
+DIYitem = input("Name a DIY item > ") 
+
+print("Menu")
+print(method,name,"with",describe,plant,"on a bed of",DIYitem)
